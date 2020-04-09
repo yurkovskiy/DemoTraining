f69e4487a159da533c6f65cce0958b00300e1578
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2020-04-09 11:54:00 +0300

    new changes

--- justfile.js
+++ justfile.js
@@ -2,6 +2,7 @@ function sayHello() {
     console.log("Hello World dear Vasis Pupkinus");
     console.log("Hello BoSS!");
     console.log("Just for testing from VS Code");
+    console.log("Another Commit");
 }
 
 sayHello();
